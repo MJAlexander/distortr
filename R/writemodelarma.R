1561ae6b139cdc9362c34fb0127a201aa0b61f32
mjalexander <monicaalexander@berkeley.edu>
2017-05-08 22:28:07 -0400

    finshing fixing write arma model

--- R/writemodelarma.R
+++ R/writemodelarma.R
@@ -159,18 +159,21 @@ writeModelARMA <- function( # Write JAGS model out as a .txt file
         e.ct[c,1] ~ dnorm(sigma.ar[c]^2/eta[c]*mu.ct[c,1], 1/(sigma.ar[c]^2*(1-sigma.ar[c]^2/eta[c])))
         for (t in 2:nyears.c[c]){
         e.ct[c,t] ~ dnorm(0, tau.ar[c])
-        mu.ct[c,t] <- rho[c]*mu.ct[c,t-1] - theta[c]*e.ct[c,t-1] + e.ct[c,t]
+        mu.ct[c,t] <- rho*mu.ct[c,t-1] - theta*e.ct[c,t-1] + e.ct[c,t]
         } # end t
-        rho[c] ~ dnorm(mu.rho.global, tau.rho.global)T(0,1)
-        theta[c] ~ dnorm(mu.theta.global, tau.theta.global)T(-1,0)
         eta[c] <- pow(sqrteta[c],2)
         sqrteta[c] <- exp(logsqrteta[c])
         logsqrteta[c] ~ dnorm(chi.eta[region.c[c]], tau.eta[region.c[c]])
-        beta[c] ~ dnorm(mu.beta.global,tau.beta.global)
-        sigma.ar[c] <- sqrt(eta[c] /((1-2*rho[c]*theta[c] + theta[c]^2)/(1-rho[c]^2)))
+        beta[c] ~ dnorm(mu.beta[region.c[c]],tau.beta[region.c[c]])
+        sigma.ar[c] <- sqrt(eta[c] /((1-2*rho*theta + theta^2)/(1-rho^2)))
         tau.ar[c] <- pow(sigma.ar[c], -2)
   } # end c
         for(r in 1:nregions){
+        # hierarchy for betas
+        mu.beta[r] ~ dnorm(mu.beta.global, tau.beta.global)
+        tau.beta[r] <- pow(sigma.beta[r], -2)
+        sigma.beta[r] ~ dunif(0, 40)
+
         # hierarchy for smoother
         chi.eta[r] ~ dnorm(mu.chi.global, tau.chi.global)
         tau.eta[r] <- pow(sigma.eta[r], -2)
@@ -181,14 +184,6 @@ writeModelARMA <- function( # Write JAGS model out as a .txt file
         tau.beta.global <- pow(sigma.beta.global, -2)
         sigma.beta.global ~ dunif(0, 40)
 
-        mu.rho.global ~ dunif(0,1)
-        tau.rho.global <- pow(sigma.rho.global, -2)
-        sigma.rho.global ~ dunif(0, 40)
-
-        mu.theta.global ~ dunif(-1,0)
-        tau.theta.global <- pow(sigma.theta.global, -2)
-        sigma.theta.global ~ dunif(0, 40)
-
         mu.chi.global ~ dnorm(0, 0.01)
         tau.chi.global <- pow(sigma.chi.global, -2)
         sigma.chi.global ~ dunif(0, 40)
@@ -198,28 +193,28 @@ writeModelARMA <- function( # Write JAGS model out as a .txt file
   if(!cs.arma&!cs.smoothing){
     cat("
         mu.ct[c,1] ~ dnorm(beta[c], 1/eta)
-        e.ct[c,1] ~ dnorm(sigma.ar[c]^2/eta*mu.ct[c,1], 1/(sigma.ar[c]^2*(1-sigma.ar[c]^2/eta)))
+        e.ct[c,1] ~ dnorm(sigma.ar^2/eta*mu.ct[c,1], 1/(sigma.ar^2*(1-sigma.ar^2/eta)))
         for (t in 2:nyears.c[c]){
-        e.ct[c,t] ~ dnorm(0, tau.ar[c])
-        mu.ct[c,t] <- rho[c]*mu.ct[c,t-1] - theta[c]*e.ct[c,t-1] + e.ct[c,t]
+        e.ct[c,t] ~ dnorm(0, tau.ar)
+        mu.ct[c,t] <- rho*mu.ct[c,t-1] - theta*e.ct[c,t-1] + e.ct[c,t]
         } # end t
-        rho[c] ~ dnorm(mu.rho.global, tau.rho.global)T(0,1)
-        theta[c] ~ dnorm(mu.theta.global, tau.theta.global)T(-1,0)
-        beta[c] ~ dnorm(mu.beta.global,tau.beta.global)
-        sigma.ar[c] <- sqrt(eta /((1-2*rho[c]*theta[c] + theta[c]^2)/(1-rho[c]^2)))
-        tau.ar[c] <- pow(sigma.ar[c], -2)
+        beta[c] ~ dnorm(mu.beta[region.c[c]],tau.beta[region.c[c]])
   } # end c
+  for(r in 1:nregions){
+        # hierarchy for betas
+        mu.beta[r] ~ dnorm(mu.beta.global, tau.beta.global)
+        tau.beta[r] <- pow(sigma.beta[r], -2)
+        sigma.beta[r] ~ dunif(0, 40)
+
+    } # end r
+        sigma.ar <- sqrt(eta /((1-2*rho*theta + theta^2)/(1-rho^2)))
+        tau.ar <- pow(sigma.ar, -2)
         mu.beta.global ~ dnorm(0, 0.01)
         tau.beta.global <- pow(sigma.beta.global, -2)
         sigma.beta.global ~ dunif(0, 40)
 
-        mu.rho.global ~ dunif(0,1)
-        tau.rho.global <- pow(sigma.rho.global, -2)
-        sigma.rho.global ~ dunif(0, 40)
-
-        mu.theta.global ~ dunif(-1,0)
-        tau.theta.global <- pow(sigma.theta.global, -2)
-        sigma.theta.global ~ dunif(0, 40)
+        rho ~ dunif(0, 1)
+        theta ~ dunif(-1, 0)
 
         eta <- pow(sqrteta, 2)
         sqrteta ~ dunif(0, 40)

12ab1c3f8b2ef46017aab0711af08e13d308bafc
mjalexander <monicaalexander@berkeley.edu>
2017-05-15 17:57:07 -0400

    changed to be tibble indexing

--- R/imputese.R
+++ R/imputese.R
@@ -21,14 +21,14 @@ imputeSE <- function(d,
                      se.column,
                      mult.factor = 1.5,
                      impute.value = 0.1){
-  isos <- unique(d[,iso.column])
+  isos <- unique(d[[iso.column]])
   # fill in miss SEs based on max within each country. If that does not exist, choose an imputation value
-  d$se_final <- d[,se.column]
+  d$se_final <- d[[se.column]]
   d$se_imputed <- 0 # binary
   for(i in 1:length(isos)){
     if(sum(d[d$iso==isos[i],data.column], na.rm = T)!=0){ #if there are data
       if(sum(is.na(d[d$iso == isos[i],se.column]))!=0){ # if there are ses missing
-        if(sum(is.na(d[d$iso == isos[i],se.column]))<length(d[d$iso == isos[i],se.column])){ #if there are some observed SEs
+        if(sum(is.na(d[d$iso == isos[i],se.column]))<length(d[d$iso == isos[i],se.column][[1]])){ #if there are some observed SEs
           d[d$iso == isos[i]&is.na(d$se),"se_final"] <- max(d[d$iso == isos[i],se.column], na.rm=T)*mult.factor
           d[d$iso == isos[i]&is.na(d$se),"se_imputed"] <- 1
         }
@@ -40,5 +40,5 @@ imputeSE <- function(d,
     }
   }
   d$se_final[d$se_final==0] <- 0.001
-  return(d[,c("se_final", "se_imputed")])
+  return(d)
 }

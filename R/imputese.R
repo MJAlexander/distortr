273439241d2d2c2ac1f28c30ce3944ac9747fb39
mjalexander <monicaalexander@berkeley.edu>
2017-05-15 21:46:52 -0400

    added two types of ses

--- R/imputese.R
+++ R/imputese.R
@@ -19,21 +19,26 @@ imputeSE <- function(d,
                      iso.column,
                      data.column,
                      se.column,
+                     transform.se.column = NULL,
                      mult.factor = 1.5,
-                     impute.value = 0.1){
+                     impute.value = 0.025,
+                     transform.impute.value = 0.1){
   isos <- unique(d[[iso.column]])
   # fill in miss SEs based on max within each country. If that does not exist, choose an imputation value
   d$se_final <- d[[se.column]]
+  d$tr_se_final <- d[[transform.se.column]]
   d$se_imputed <- 0 # binary
   for(i in 1:length(isos)){
     if(sum(d[d$iso==isos[i],data.column], na.rm = T)!=0){ #if there are data
       if(sum(is.na(d[d$iso == isos[i],se.column]))!=0){ # if there are ses missing
         if(sum(is.na(d[d$iso == isos[i],se.column]))<length(d[d$iso == isos[i],se.column][[1]])){ #if there are some observed SEs
           d[d$iso == isos[i]&is.na(d$se),"se_final"] <- max(d[d$iso == isos[i],se.column], na.rm=T)*mult.factor
+          d[d$iso == isos[i]&is.na(d$se),"tr_se_final"] <- max(d[d$iso == isos[i],transform.se.column], na.rm=T)*mult.factor
           d[d$iso == isos[i]&is.na(d$se),"se_imputed"] <- 1
         }
         else{ # fill with pre-chosen value
           d[d$iso == isos[i]&is.na(d$se),"se_final"] <- impute.value
+          d[d$iso == isos[i]&is.na(d$se),"tr_se_final"] <- transform.impute.value
           d[d$iso == isos[i]&is.na(d$se),"se_imputed"] <- 1
         }
       }
